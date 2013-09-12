f7f2af691aeec407157a997b141361bcc5257245
zenry <henry@postb.us>
2013-09-12 15:25:25 +0200

    added button to real L4 docs

--- views/page.html.php
+++ views/page.html.php
@@ -1,5 +1,4 @@
 <?php if(is_array($content)) : ?>
-
 	<?php foreach($content as $item) : ?>
 	<ul>
 		<li><a href="<?php echo $item['path'].'?string='.params('string'); ?>"><?php echo $item['name']; ?></a></li>
@@ -7,5 +6,7 @@
 	<?php endforeach; ?>
 
 <?php else : ?>
+	<a target="_blank" href="http://laravel.com/docs/<?php echo($path); ?>">Laravel.com/docs/<?php echo ucfirst($path); ?></a>
+	<hr />
 	<?php echo $content; ?>
 <?php endif; ?>
